3ce859c4ea287f157541640cbf4d5edf88bfef5c
CamDawg <pcamagna@yahoo.com>
2026-04-03 10:10:08 -0700

    v17 prep

--- package_mod.bat
+++ package_mod.bat
@@ -6,7 +6,7 @@ call "%~dp0..\ModPackaging\utilities\ie_games.bat"
 
 REM /* MODIFY: set the values of the 3 variables below to reflect the current mod version */
 set "mod_name=SP Collection"
-set mod_version=v16
+set mod_version=v17
 set mod_folder=spstuff
 
 REM /* MODIFY: list here which IE games the mod is compatible with, from this list of possibilities: */
@@ -22,6 +22,8 @@ REM /* the mod_readme link should point to the online readme and work fine as-is
 set ico_folder=%mod_folder%\style
 set audio_folder=%mod_folder%\audio
 set tispack_folder=%mod_folder%\tiz
+set tile2ee_folder=%mod_folder%\tools\tile2ee
+set tileconv_folder=%mod_folder%\tools\tileconv
 set iconv_folder=%mod_folder%\lang\iconv
 set mod_readme=http://www.gibberlings3.net/readmes/readme-%mod_folder%.html
 
